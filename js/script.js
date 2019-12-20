31b93264579d17204134b08f0e52e95b31773337
lil L <57925562+leolopez10@users.noreply.github.com>
2019-12-20 13:18:01 -0600

    javascript updated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 //Global Variables
-var searchTerm = $(".search-term");
+var searchInput = $("#search-input").val();
 var searchBtn = $("#searchBtn");
 //Create an on click event for pulling information from the new york times based on year and end date
 
+console.log(searchInput);
+
 searchBtn.on("click", function(){
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=b2295df2-2444-4aa9-a872-0333546237e7"
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchInput + "&api-key=b2295df2-2444-4aa9-a872-0333546237e7"
 
     $.ajax({
         url: queryURL,
@@ -13,7 +15,7 @@ searchBtn.on("click", function(){
         console.log(response);
 
         //create a div block named resultDiv
-        var resultDiv =  $("<div>");
+        // var resultDiv =  $("<div>");
         //create a variable for artical title
         
         //create a variable for author of article
